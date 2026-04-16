755becba5a74bd91a2ec192337f5641cff9c5141
Harsh-2803 <rupharsh07@gmail.com>
2026-04-16 15:58:44 +0530

    UC2:Feet and Inches measurement

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,23 +1,62 @@
+class Feet {
+
+    private double value;
+
+    public Feet(double value) {
+        if (Double.isNaN(value)) {
+            throw new IllegalArgumentException("Invalid Feet value");
+        }
+        this.value = value;
+    }
+
+    public boolean equals(Feet other) {
+        return Double.compare(this.value, other.value) == 0;
+    }
+}
+
+class Inches {
+
+    private double value;
+
+    public Inches(double value) {
+        if (Double.isNaN(value)) {
+            throw new IllegalArgumentException("Invalid Inches value");
+        }
+        this.value = value;
+    }
+
+    public boolean equals(Inches other) {
+        return Double.compare(this.value, other.value) == 0;
+    }
+}
+
 public class QuantityMeasurementApp {
 
-    public boolean areEqual(double value1, double value2) {
+    public static boolean compareFeet(double v1, double v2) {
+        Feet f1 = new Feet(v1);
+        Feet f2 = new Feet(v2);
+        return f1.equals(f2);
+    }
 
-        if (Double.isNaN(value1) || Double.isNaN(value2)) {
-            throw new IllegalArgumentException("Invalid input: Not a number");
-        }
-
-        return Double.compare(value1, value2) == 0;
+    public static boolean compareInches(double v1, double v2) {
+        Inches i1 = new Inches(v1);
+        Inches i2 = new Inches(v2);
+        return i1.equals(i2);
     }
 
     public static void main(String[] args) {
 
-        QuantityMeasurementApp app = new QuantityMeasurementApp();
-
         double feet1 = 5.0;
         double feet2 = 5.0;
 
-        boolean result = app.areEqual(feet1, feet2);
+        double inch1 = 12.0;
+        double inch2 = 12.0;
 
-        System.out.println("Are both measurements equal? " + result);
+        boolean feetResult = compareFeet(feet1, feet2);
+
+        boolean inchResult = compareInches(inch1, inch2);
+
+        System.out.println("Feet equal? " + feetResult);
+        System.out.println("Inches equal? " + inchResult);
     }
 }
